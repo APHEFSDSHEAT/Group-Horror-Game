0f5c925626cfda705d705570473b0d66d10c3451
Kirboisverycool <1522838@edu.burnabyschools.ca>
2023-05-16 14:57:40 -0700

    :)

---
+++ Group Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] public float speed = 10f;
+    [SerializeField] public float jumpPower = 15f;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] Transform feet;
+
+    float mx;
+    bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mx = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+        CheckGrounded();
+
+        if (rb.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        else if (rb.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(mx * speed, rb.velocity.y);
+    }
+
+    void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
+    }
+
+    void CheckGrounded()
+    {
+        if (Physics2D.OverlapCircle(feet.position, 0.5f, groundLayer))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+}
